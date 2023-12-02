05b436d001c72f60cf10363411f7736121dd289b
weberjr1507 <weberjr1507@gmail.com>
2023-12-01 21:10:04 -0600

    added input scrubber

--- PortReader.py
+++ PortReader.py
@@ -1,5 +1,6 @@
 import serial
 ser = serial.Serial('/dev/ttyACM0')
+import re
 ser.flushInput()
 
 while True:
@@ -7,7 +8,16 @@ while True:
         ser_bytes = ser.readline()
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         print(decoded_bytes)
+        ser_bytes = re.sub("[b,\r,\n]","",ser_bytes)
+        outputFile = open("arduinolog.txt","a")
+        outputFile.write(ser_bytes)
+        outputFile.write("\n")
+        outputFile.close
     except:
         print("Keyboard Interrupt")
         break
-    print("hello!")
+
+
+# testinput = 'b1 0 1044346547\r\n'
+
+
